0ff56853a08b6301531a6bff375d311e18311b8e
rajnish <rajnishmsrit@gmail.com>
2021-01-28 02:52:04 -0800

    dev : daily update -- medium

---
+++ Leetcode/String/valid-anagram.cpp
@@ -0,0 +1,30 @@
+/**
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
+ */
+# include <iostream>
+# include <bits/stdc++.h>
+# include <vector>
+# include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+    }
+};
+
+void test(){
+
+    string s = "alpha";
+    string t = "aphla";
+    cout << "Program starts \n";
+    Solution s;
+    cout << "The no is: " << endl;
+}
+
+int main(){
+    test();
+    return 0;
+}
