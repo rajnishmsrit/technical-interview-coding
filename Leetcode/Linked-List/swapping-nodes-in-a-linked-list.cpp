e8979b1d153195273715680acb42fd3b838e1c04
rajnish <rajnishmsrit@gmail.com>
2021-02-01 14:35:16 -0800

    dev : updated -- easy

---
+++ Leetcode/Linked-List/swapping-nodes-in-a-linked-list.cpp
@@ -0,0 +1,23 @@
+# include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        
+    }
+};
+
+void test(){
+    vector<int> v{1,1,2};
+    cout << "Program starts \n";
+    Solution s;
+    s.runningSum(v);
+    cout << "The no is: " << endl;
+}
+
+int main(){
+    test();
+    return 0;
+}
