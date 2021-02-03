801c1906f9a8d6adf25707d7a9c59af426693ab2
rajnish <rajnishmsrit@gmail.com>
2021-02-03 12:54:13 -0800

    dev : daily challenge

--- Leetcode/Array/n-th-tribonacci-number.cpp
+++ Leetcode/Array/n-th-tribonacci-number.cpp
@@ -7,9 +7,9 @@ class Solution {
 public:
     int arr[38]={0};
     int tribonacci(int n) {
-        arr[0] = 0;
+        
         if(n==0) return 0;
-        arr[1] = arr[2] = 1;
+        if(n==1 && n==2) return 1;
         if (arr[n]==0){
             arr[n] = tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
         }
@@ -21,7 +21,7 @@ void test(){
     vector<int> v{1,1,2};
     cout << "Program starts \n";
     Solution s;
-    cout << "The no is: " << s.tribonacci(36) << endl;
+    cout << "The no is: " << s.tribonacci(32) << endl;
 }
 
 int main(){
